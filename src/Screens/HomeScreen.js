18ab63a8798ddc46fdfb66eae82d0b6c669ac575
Suhad Mendis <Suhad.a.mendis@gmail.com>
2024-06-30 13:34:57 +0530

    initial commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sample component!</Text>
+    </View>
+  );
+};
+
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 20,
+    color: '#333',
+  },
+});
